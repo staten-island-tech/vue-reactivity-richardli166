8de10333a375d6d878fec07b31373d3ff778aa97
richardli166 <144455838+richardli166@users.noreply.github.com>
2024-02-14 11:27:48 -0500

    THIS WAS STUPID MSITAKE

--- vue-project/src/components/TetrisCard.vue
+++ vue-project/src/components/TetrisCard.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <h2>{{ items.name }}</h2>
-        <h3>{{ items.price }}</h3>
-        <img :src="items.img" alt= {{ items.name }}/>
+        <h2>{{ props.items.name }}</h2>
+        <h3>{{ props.items.price }}</h3>
+        <img :src="props.items.img" alt="{{ props.items.name }}"/>
         <h2>{{ clicked }}</h2>
         <button @click="increment">Add to Your cart</button>
     </div>
@@ -13,7 +13,7 @@
         const props = defineProps({
         items: Object, 
         });
-        console.log(items.name);
+        console.log(props.items.name);
 //clicker logic 
     const clicked = ref(0);
     function increment(){
