f805a3cf960b07f3d1e6eb2d9b225443e375be69
richardli166 <144455838+richardli166@users.noreply.github.com>
2024-02-26 09:37:44 -0500

    channnngggeeed ahhhhhhhh.

--- vue-project/src/stores/allvar.js
+++ vue-project/src/stores/allvar.js
@@ -1,23 +1,16 @@
-import { reactive } from 'vue'
+import { ref } from 'vue';
 
-export const store = reactive({
-    cart: [],
+export const store = {
+    cart: ref([]),
     addTetris(name, price, imagesrc, amount) {
-        for (let i = 0; i < this.cart.length; ++i) {
-             if (this.cart[i].name == name) {
-                 this.cart[i].amount = amount;
-                 return;
-             }
+        const existingItem = store.cart.value.find(item => item.name === name);
+        if (existingItem) {
+            existingItem.amount = amount;
+        } else {
+            store.cart.value = [...store.cart.value, { name, price, imagesrc, amount }];
         }
-
-        this.cart.push({
-            "name": name,
-            "price": price,
-            "imagesrc": imagesrc,
-            "amount": amount,
-        })
     },
     getCart() {
-        return this.cart;
+        return store.cart.value;
     }
-});
+};
